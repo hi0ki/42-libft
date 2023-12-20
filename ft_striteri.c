d36bd8c05eda4128be18ff053b2fe244f3ae7a38
hi0ki <ansarimehdi518@gmail.com>
2023-12-21 00:54:29 +0100

    last push from school

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,34 @@
 /*   By: eel-ansa <eel-ansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 23:19:58 by eel-ansa          #+#    #+#             */
-/*   Updated: 2023/12/21 00:29:25 by eel-ansa         ###   ########.fr       */
+/*   Updated: 2023/12/21 00:53:30 by eel-ansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// void f(unsigned int index, char *c)
+// {
+//     *c = ft_toupper(*c);
+// }
+
+
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        f(i, &s[i]);
+        i++;
+    } 
 }
+
+// int main()
+// {
+
+//     char str[] = "hello";
+//     ft_striteri(str, f);
+//     printf("%s", str);
+// }
