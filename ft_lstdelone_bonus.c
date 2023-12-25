fb24a60647958d7108aa25817ba231810fe6e1f4
hi0ki <ansarimehdi518@gmail.com>
2023-12-25 11:07:26 +0100

    last puch with bonus

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eel-ansa <eel-ansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 22:53:11 by eel-ansa          #+#    #+#             */
-/*   Updated: 2023/12/25 10:58:25 by eel-ansa         ###   ########.fr       */
+/*   Updated: 2023/12/25 11:06:03 by eel-ansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 // }
 void ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-    lst->content = NULL;
     del(lst->content);
+    free(lst);
 }
 
 // int main()
